62b46d2fc21f5c8d11ac8e93b7797b7b4afa15a4
Anthony Allen <Anthony_Allen@comcast.com>
2020-04-14 13:05:55 -0400

    One step closer. Now only all lower case has bug

---
+++ acronym/src/Acronym.hs
@@ -0,0 +1,38 @@
+module Acronym (abbreviate) where
+
+import Data.List.Split(splitOn, linesBy)
+import Data.Char(isUpper, toUpper)
+import Text.Casing(unIdentifier, fromHumps)
+
+allCaps :: String
+allCaps = "GNU Image Manipulation Program" -- CCW
+
+ruby :: String
+ruby = "ruby on rails"
+
+isAllUpper :: String -> Bool
+isAllUpper xs = (length $ filter isUpper xs) ==  length xs
+
+flattenAcro :: String -> String
+flattenAcro xs
+  | isAllUpper xs == True = [head xs]
+  | otherwise             = xs
+
+removeAcros :: String -> String
+removeAcros xs = concatMap flattenAcro (words xs)
+
+abbreviate :: String -> String
+--abbreviate = undefined
+abbreviate xs  = map (toUpper . head) finalSet
+  where
+    finalSet = concatMap words (unIdentifier $ fromHumps $ removeAcros  xs)
+
+
+
+-- isHumpedCase --> has lowerCase next to Uppercase
+-- isAcronym    --> has a word of all caps
+-- isDashCasing --> has a word with a hyphen-casing
+
+-- TODO -> Either:
+-- -- a) Modularize all the specific acro makers, make some predicates, make giant switch/PM statement
+-- -- b) Stay on this road and figure out how to prevent removeAcros from folding ""ALL LOWER CASE!!""
